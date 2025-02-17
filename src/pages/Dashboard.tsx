23542eef5fa8973c63855f60ebdeae0534b785fe
iamnubio <iamnubiomusic@gmail.com>
2025-02-17 13:56:14 -0500

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,3 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
 export function Dashboard() {
   const [news, setNews] = useState([]);
   const [statistics, setStatistics] = useState([]);
